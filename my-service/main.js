d6fb0d2aa797d359881953489677f910103882c9
DeutscherFuchs1987 <diob9092@gmail.com>
2026-03-25 05:42:54 +0000

    v.1.2

--- my-service/main.js
+++ my-service/main.js
@@ -4,8 +4,32 @@ const jwt = require('jsonwebtoken');
 const bcr = require('bcrypt');
 const app = express();
 const SECRET = "mysecretkey";
-
 app.use(express.json());
+
+
+const auth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) return res.status(401).json({ message: "Authorization header is missing" });
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) return res.status(401).json({ error: "Token is missing" });
+
+    try {
+        const decoded = jwt.verify(token, SECRET);
+        req.user = decoded;
+        next();
+
+    } catch (error) {
+        console.error(error);
+        return res.status(401).json({ error: "Invalid token" });
+    }
+
+}
+
+
+
 
 app.get("/users", (req, res) => {
     const users = db.prepare("SELECT * FROM users").all();
@@ -28,7 +52,7 @@ app.post("/auth/register", (req, res) => {
         const { password: _, ...safeUser } = newUser;
         res.status(201).json(safeUser);
 
-    } catch (error){
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal Server Error" });
     }
@@ -41,23 +65,36 @@ app.post("/auth/login", (req, res) => {
     try {
         if (!username || !password) return res.status(400).json({ message: "Username and password are required" });
 
-            const user = db.prepare("SELECT * FROM users WHERE username = ?").get(username);
-            if (!user) return res.status(401).json({ message: "Invalid credentials" });
+        const user = db.prepare("SELECT * FROM users WHERE username = ?").get(username);
+        if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
-            const hashPassword = bcr.compareSync(password, user.password);
-            if (!hashPassword) return res.status(401).json({ message: "Invalid credentials" });
+        const hashPassword = bcr.compareSync(password, user.password);
+        if (!hashPassword) return res.status(401).json({ message: "Invalid credentials" });
 
-            const { password: _, ...safeUser } = user;
-            const token = jwt.sign(safeUser, SECRET, { expiresIn: "24h" });
-            return res.status(200).json({success: true, token, error: null});
+        const { password: _, ...safeUser } = user;
+        const token = jwt.sign(safeUser, SECRET, { expiresIn: "24h" });
+        return res.status(200).json({ success: true, token, error: null });
 
-    } catch (error){
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 });
 
 
+
+app.get("/auth/profile", auth, (req, res) => {
+    const user = db.prepare("SELECT * FROM users WHERE id = ?").get(req.user.id);
+    try {
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        const { password: _, ...safeUser } = user;
+        res.status(200).json(safeUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
 
 
 
